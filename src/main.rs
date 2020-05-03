1d4a3588f087e3bc6c7887f3f60100d2904f311c
Jukka Kelanne <jukka.kelanne@gmail.com>
2020-05-03 14:43:00 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,81 @@
-use psutil::{cpu, Percent};
-use std::{thread, time, error::Error};
+use psutil::{cpu, sensors};
+use std::{time, error::Error};
 use std::io::{Write, stdout};
 use crossterm::{
-    event::{poll, read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    ExecutableCommand, 
+    event::{poll, read, Event, KeyCode},
     cursor, 
     terminal,
     execute,
+    ErrorKind
 };
 use tui::{
     backend::TermionBackend,
     layout::{Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    widgets::{Block, Borders, Gauge, Widget},
+    style::{Color, Style},
+    widgets::{Block, Borders, Gauge},
     Terminal,
 };
 
-use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
-
-fn setup_terminal() -> std::io::Result<()> {
-    Ok(())
-}
+//use termion::{event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen};
 
 fn get_gauge<'a>(title: &'a str, borders_style: Borders, ratio: f64) -> Gauge<'a> {
     Gauge::default()
         .block(Block::default().title(title).borders(borders_style))
         .style(Style::default().fg(Color::Green))
-        //.ratio((avg_percents[0] / 100.0) as f64);
         .ratio(ratio)
-    
-//    gauge;
+}
+
+fn terminal_setup() -> std::result::Result<(), ErrorKind> {
+    let mut stdout = stdout();
+    execute!(stdout, terminal::EnterAlternateScreen)?;
+    execute!(stdout, cursor::Hide)?;
+    execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
+    terminal::enable_raw_mode().unwrap();
+
+    Ok(())
+}
+
+fn terminal_cleanup() -> std::result::Result<(), ErrorKind> {
+    let mut tmp_stdout = std::io::stdout();
+    execute!(tmp_stdout, cursor::MoveTo(0,0))?;
+    execute!(tmp_stdout, terminal::Clear(terminal::ClearType::All))?;
+    execute!(tmp_stdout, terminal::LeaveAlternateScreen)?;
+    execute!(tmp_stdout, cursor::Show).unwrap();
+    terminal::disable_raw_mode().unwrap();
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut collector: cpu::CpuPercentCollector = cpu::CpuPercentCollector::new().unwrap();
-    let mut avg_percents: Vec<Percent> = Vec::new();
 
-    let mut stdout = stdout();
-    execute!(stdout, terminal::EnterAlternateScreen).unwrap();
-    execute!(stdout, cursor::Hide).unwrap();
-    execute!(stdout, terminal::Clear(terminal::ClearType::All)).unwrap();
+    let logical_cpu_count = cpu::cpu_count();
+    let physical_cpu_count = cpu::cpu_count_physical();
 
-    let backend = TermionBackend::new(stdout);
+    let temperatures = sensors::temperatures();
+
+    terminal_setup()?;
+
+    let backend = TermionBackend::new(std::io::stdout());
     let mut terminal = Terminal::new(backend)?;
 
     let borders_style = Borders::ALL;
     //let borders_style = Borders::BOTTOM | Borders::LEFT;
 
     loop {
-        avg_percents = collector.cpu_percent_percpu().unwrap();
+        let avg_percents = collector.cpu_percent_percpu().unwrap();
         let avg_total = collector.cpu_percent().unwrap();
-        if poll(time::Duration::from_millis(100))? {
+        if poll(time::Duration::from_millis(1000))? {
             let event = read()?;
             if event == Event::Key(KeyCode::Char('q').into()) {
                 println!("Exiting!!");
-                terminal.show_cursor()?;
-
-                terminal::disable_raw_mode().unwrap();
+                terminal_cleanup()?;
+                
+                println!("Logical Units: {}; Physical Units: {}", logical_cpu_count, physical_cpu_count);
+                for t in temperatures {
+                    let t_unwrapped = t.unwrap();
+                    println!("{:?}: {}", t_unwrapped.label(), t_unwrapped.current().celsius());
+                }
                 break;
             }
         }
@@ -89,36 +108,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                         ].as_ref())
                     .split(chunks[0]);
 
-/*                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU0").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[0] / 100.0) as f64);*/
-                //f.render_widget(gauge, chunks[0]);
                 f.render_widget(get_gauge("CPU0", borders_style, (avg_percents[0] / 100.0) as f64), chunks[0]);
-
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU1").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[1] / 100.0) as f64);
-                f.render_widget(gauge, chunks[1]);                
-                
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU2").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[2] / 100.0) as f64);
-                f.render_widget(gauge, chunks[2]);                
-                
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU3").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[3] / 100.0) as f64);
-                f.render_widget(gauge, chunks[3]);
-
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU4").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[4] / 100.0) as f64);
-                f.render_widget(gauge, chunks[4]);
+                f.render_widget(get_gauge("CPU1", borders_style, (avg_percents[1] / 100.0) as f64), chunks[1]);
+                f.render_widget(get_gauge("CPU2", borders_style, (avg_percents[2] / 100.0) as f64), chunks[2]);
+                f.render_widget(get_gauge("CPU3", borders_style, (avg_percents[3] / 100.0) as f64), chunks[3]);
+                f.render_widget(get_gauge("CPU4", borders_style, (avg_percents[4] / 100.0) as f64), chunks[4]);
 
             }
             {
@@ -134,36 +128,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                         ].as_ref())
                     .split(chunks[1]);
 
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU5").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[5] / 100.0) as f64);
-                f.render_widget(gauge, chunks[0]);
-
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU6").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[6] / 100.0) as f64);
-                f.render_widget(gauge, chunks[1]);                
-                
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU7").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[7] / 100.0) as f64);
-                f.render_widget(gauge, chunks[2]);                
-                
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU8").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[8] / 100.0) as f64);
-                f.render_widget(gauge, chunks[3]);
-
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU9").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[9] / 100.0) as f64);
-                f.render_widget(gauge, chunks[4]);
-
+                f.render_widget(get_gauge("CPU5", borders_style, (avg_percents[5] / 100.0) as f64), chunks[0]);
+                f.render_widget(get_gauge("CPU6", borders_style, (avg_percents[6] / 100.0) as f64), chunks[1]);
+                f.render_widget(get_gauge("CPU7", borders_style, (avg_percents[7] / 100.0) as f64), chunks[2]);
+                f.render_widget(get_gauge("CPU8", borders_style, (avg_percents[8] / 100.0) as f64), chunks[3]);
+                f.render_widget(get_gauge("CPU9", borders_style, (avg_percents[9] / 100.0) as f64), chunks[4]);
             }
             {
                 let chunks = Layout::default()
@@ -177,35 +146,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                         ].as_ref())
                     .split(chunks[2]);
 
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU10").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[10] / 100.0) as f64);
-                f.render_widget(gauge, chunks[0]);
-
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU11").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[11] / 100.0) as f64);
-                f.render_widget(gauge, chunks[1]);                
-                
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU12").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[12] / 100.0) as f64);
-                f.render_widget(gauge, chunks[2]);                
-                
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU13").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[13] / 100.0) as f64);
-                f.render_widget(gauge, chunks[3]);
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU14").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[14] / 100.0) as f64);
-                f.render_widget(gauge, chunks[4]);
-
+                f.render_widget(get_gauge("CPU10", borders_style, (avg_percents[10] / 100.0) as f64), chunks[0]);
+                f.render_widget(get_gauge("CPU11", borders_style, (avg_percents[11] / 100.0) as f64), chunks[1]);
+                f.render_widget(get_gauge("CPU12", borders_style, (avg_percents[12] / 100.0) as f64), chunks[2]);
+                f.render_widget(get_gauge("CPU13", borders_style, (avg_percents[13] / 100.0) as f64), chunks[3]);
+                f.render_widget(get_gauge("CPU14", borders_style, (avg_percents[14] / 100.0) as f64), chunks[4]);
             }
             {
                 let chunks = Layout::default()
@@ -219,36 +164,11 @@ fn main() -> Result<(), Box<dyn Error>> {
                         ].as_ref())
                     .split(chunks[3]);
 
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU15").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[15] / 100.0) as f64);
-                f.render_widget(gauge, chunks[0]);
-
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU16").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[16] / 100.0) as f64);
-                f.render_widget(gauge, chunks[1]);                
-                
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU17").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[17] / 100.0) as f64);
-                f.render_widget(gauge, chunks[2]);                
-                
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU18").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[18] / 100.0) as f64);
-                f.render_widget(gauge, chunks[3]);                
-
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU19").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[19] / 100.0) as f64);
-                f.render_widget(gauge, chunks[4]);
-
+                f.render_widget(get_gauge("CPU15", borders_style, (avg_percents[15] / 100.0) as f64), chunks[0]);
+                f.render_widget(get_gauge("CPU16", borders_style, (avg_percents[16] / 100.0) as f64), chunks[1]);
+                f.render_widget(get_gauge("CPU17", borders_style, (avg_percents[17] / 100.0) as f64), chunks[2]);
+                f.render_widget(get_gauge("CPU18", borders_style, (avg_percents[18] / 100.0) as f64), chunks[3]);
+                f.render_widget(get_gauge("CPU19", borders_style, (avg_percents[19] / 100.0) as f64), chunks[4]);
             }
             {
                 let chunks = Layout::default()
@@ -262,68 +182,15 @@ fn main() -> Result<(), Box<dyn Error>> {
                         ].as_ref())
                     .split(chunks[4]);
 
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU20").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[20] / 100.0) as f64);
-                f.render_widget(gauge, chunks[0]);
-
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU21").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[21] / 100.0) as f64);
-                f.render_widget(gauge, chunks[1]);
-
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU22").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[22] / 100.0) as f64);
-                f.render_widget(gauge, chunks[2]);
-
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU23").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_percents[23] / 100.0) as f64);
-                f.render_widget(gauge, chunks[3]);
-
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("AVG").borders(borders_style))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio((avg_total / 100.0) as f64);
-                f.render_widget(gauge, chunks[4]);
-
-
-/*                
-                let gauge = Gauge::default()
-                    .block(Block::default().title("CPU6").borders(Borders::ALL))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio(0.33);
-                f.render_widget(gauge, chunks[2]);                
-                
-                let gauge = Gauge::default()                    
-                    .block(Block::default().title("CPU7").borders(Borders::ALL))
-                    .style(Style::default().fg(Color::Green))
-                    .ratio(0.33);
-                f.render_widget(gauge, chunks[3]);
-*/
+                f.render_widget(get_gauge("CPU20", borders_style, (avg_percents[0] / 100.0) as f64), chunks[0]);
+                f.render_widget(get_gauge("CPU21", borders_style, (avg_percents[1] / 100.0) as f64), chunks[1]);
+                f.render_widget(get_gauge("CPU22", borders_style, (avg_percents[2] / 100.0) as f64), chunks[2]);
+                f.render_widget(get_gauge("CPU23", borders_style, (avg_percents[3] / 100.0) as f64), chunks[3]);
+                f.render_widget(get_gauge("AVG", borders_style, (avg_total / 100.0) as f64), chunks[4]);
             }
-
         })?;
         
-        thread::sleep(time::Duration::from_secs(1));
-/*        if let Err(e) = check_events() {
-            println!("Error: {:?}", e);
-        }*/
+        //thread::sleep(time::Duration::from_secs(1));
     }
-//        stdout.execute(cursor::MoveTo(0,0));
-//        stdout.execute(terminal::Clear(terminal::ClearType::All));
-
-
-//        for ac in avg_percents {
-//            println!("{:.1}%", ac);
-//
-//        }
-       
-//        stdout.flush();
     Ok(())
 }
